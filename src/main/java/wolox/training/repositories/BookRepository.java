bdfb0631fa059375e24bdc639eff661e96946666
Marcelo Guzman <marcelo.guzman@wolox.com.ar>
2020-03-02 12:41:39 -0300

    Add Book Repository with findByAuthor method

---
+++ src/main/java/wolox/training/repositories/BookRepository.java
@@ -0,0 +1,22 @@
+package wolox.training.repositories;
+
+import java.util.Optional;
+import org.springframework.data.repository.Repository;
+import wolox.training.models.Book;
+
+/**
+ * BookRepository.java Interface that serves as a Repository for the Book model.
+ *
+ * @author M. G.
+ */
+
+public interface BookRepository extends Repository<Book, Long> {
+
+    /**
+     * Find a book by its author.
+     *
+     * @param author the name of the author
+     * @return the book if it exists, and otherwise Optional.empty() object.
+     */
+    Optional<Book> findByAuthor(String author);
+}
